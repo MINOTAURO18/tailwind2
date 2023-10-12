cbe4804b84d6aa89f6f84bd197bb21576b30c50e
alexander muñoz <alexander12345my@gmail.com>
2023-10-12 18:55:34 -0500

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '600px': '600px',
+      }
+    },
+  },
+  plugins: [],
+}
